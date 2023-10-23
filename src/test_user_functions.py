9d2d1eb926fc0a6f8d965c7ff26b2b8d10a30b39
Noeddasheep <muceteo@gmail.com>
2023-10-23 06:12:26 +0100

    work in progress

--- src/test_user_functions.py
+++ src/test_user_functions.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from unittest.mock import patch
 from src.user_functions import get_travel_information, get_expense
 
+
 class TestTravelInfo(TestCase):
 
     # Check if it returns the correct value with valid inputs
